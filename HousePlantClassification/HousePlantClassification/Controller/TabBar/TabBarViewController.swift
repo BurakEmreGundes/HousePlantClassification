70eb6c678ca9405a81c7649a8b28566e4d242f4e
BurakEmreGundes <burak.gundes@ogr.sakarya.edu.tr>
2022-10-16 14:18:48 +0300

    added tab bar vc

---
+++ HousePlantClassification/HousePlantClassification/Controller/TabBar/TabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarViewController.swift
+//  HousePlantClassification
+//
+//  Created by Burak Emre gündeş on 16.10.2022.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.selectedIndex = 1
+    }
+
+
+}
